6c531cd67ee479ac4da8a0f47c3435878923ac85
Refael Levi <refael14levi@gmail.com>
2024-09-10 15:58:21 +0300

    ball change

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-
-
 function onBallClick() {
-    var ball = document.querySelector('.ball')
-    var newSize = prompt("Enter the new  size of the ball:")
+    var ball = document.querySelector('.ball');
+    var newSize = prompt("Enter the new size of the ball:");
 
-    if (isNaN(newSize)) {
-        alert("Please enter a valid number.")
-    } else if (newSize > 1000) {
-        alert("Too big!")
-    } else {
-      //  const ball = document.getElementById('resizeBall')
-        ball.style.width = newSize + 'px'
-        ball.style.height = newSize + 'px'
-        ball.style.lineHeight = newSize + 'px'
+    if (isNaN(newSize) || newSize < 0) {
+        console.log("Please enter a valid number.");
+    } else if (newSize > 400) {
+        console.log("Size too large! Resetting to 100px.");
+        newSize = 100; // Reset to 100px if the size exceeds 400px
     }
+
+    ball.style.width = newSize + 'px';
+    ball.style.height = newSize + 'px';
+    ball.style.lineHeight = newSize + 'px';
 }
