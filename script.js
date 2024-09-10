543fc8126e58fd1cfab8638fad79db8d7cbf1dd3
Refael Levi <refael14levi@gmail.com>
2024-09-10 17:34:45 +0300

    adding limit

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-function onBallClick() {
-    var ball = document.querySelector('.ball');
-    var newSize = prompt("Enter the new size of the ball:");
+// function onBallClick() {
+//     var ball = document.querySelector('.ball')
+//     var newSize = prompt("Enter the new size of the ball:")
+
+//     if (isNaN(newSize) || newSize < 0) {
+//         console.log("Please enter a valid number.")
+//     } else if (newSize === 400) {
+//         console.log("Size too large! Resetting to 100px.")
+//         newSize = 300
+//     }
+
+//     ball.style.width = newSize + 'px'
+//     ball.style.height = newSize + 'px'
+//     ball.style.lineHeight = newSize + 'px'
+//     console.log(newSize)
+    
+// }
+
+
+function onBallClick(elBall, maxDiameter) {
+    var ball = elBall
+    var newSize = prompt("Enter the new size of the ball:")
 
     if (isNaN(newSize) || newSize < 0) {
-        console.log("Please enter a valid number.");
-    } else if (newSize > 400) {
-        console.log("Size too large! Resetting to 100px.");
-        newSize = 100; // Reset to 100px if the size exceeds 400px
+        console.log("Please enter a valid number.")
+    } else if (newSize > maxDiameter) {
+        console.log(`Size too large! Resetting to 100px.`)
+        newSize = 100
     }
 
-    ball.style.width = newSize + 'px';
-    ball.style.height = newSize + 'px';
-    ball.style.lineHeight = newSize + 'px';
+    ball.style.width = newSize + 'px'
+    ball.style.height = newSize + 'px'
+    ball.style.lineHeight = newSize + 'px'
+    console.log(newSize)
 }
